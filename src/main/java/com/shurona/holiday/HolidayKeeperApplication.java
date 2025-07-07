97860e8e9553c60b7dbab4a0830b84c7f4ae66a7
shurona <khc4669@naver.com>
2025-07-07 23:29:40 +0900

    feat: Main Application

---
+++ src/main/java/com/shurona/holiday/HolidayKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.shurona.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayKeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayKeeperApplication.class, args);
+    }
+
+}

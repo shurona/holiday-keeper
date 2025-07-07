702f4b2a482a1afea0d12bd51691f3d86ce335fa
shurona <khc4669@naver.com>
2025-07-07 23:31:14 +0900

    test: test main application

---
+++ src/test/java/com/shurona/holiday/HolidayKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shurona.holiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayKeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ae9baae0a3d48f6b2c9b086db7041024c015562e
shurona <khc4669@naver.com>
2025-07-07 23:24:16 +0900

    feat: jpa audit 및 querydsl 설정

---
+++ src/main/java/com/shurona/holiday/common/configuration/JpaAuditConfiguration.java
@@ -0,0 +1,10 @@
+package com.shurona.holiday.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditConfiguration {
+
+}
